238d9787799c5e8fc6abd716cb1567b9d9b7ce6a
Low Kian Seong <kianseong@gmail.com>
2014-04-11 14:15:26 +0800

    Adding more tests to linode

--- test/test_linote.py
+++ test/test_linote.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from utils import clean_style, clean_note
 
 cur_dir = os.path.abspath(__file__).split('/')[:-2]
 par_dir = '/'.join(cur_dir)
@@ -21,11 +22,18 @@ class DefaultTestCase(unittest.TestCase):
 
     def test_version(self):
         self.assertIsNotNone(linote.__version__, '0.0.1')
+    
+    def test_utils_clean_style(self):
+        assert clean_style("hello") == "<p>hello</p>"
+
+    def test_utils_clean_note(self):
+        assert clean_note("<h1>hello</h1>") == "hello"
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(DefaultTestCase('test_version'))
+    suite.addTest(DefaultTestCase('test_utils_clean_style'))
     return suite
 
 
