d754d9c58ded27e311b9a9270d8ff062e8e8e594
Low Kian Seong <kianseong@gmail.com>
2014-05-01 11:28:48 +0800

    Adding mkdirs for parent directory for config.ini

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from utils import get_config
 dst = path('~/.linote/config.ini').expanduser()
 
 if not dst.exists():
+    dst.parent.makedirs_p()
     path('config.ini.sample').copy(dst)
 
 linote_config = get_config(path('~/.linote/config.ini').expanduser())
